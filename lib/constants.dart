fcc3ead0dd74a1dbb3f7d90f4bb39b84b5c0fdb9
Tan Dinh <tan.dinh@niteco.se>
2022-08-19 15:53:32 +0700

    create assets folder and define theme

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF2697FF);
-const secondaryColor = Color(0xFF2A2D3E);
-const bgColor = Color(0xFF212332);
+const darkPinkColor = Color.fromRGBO(226, 104, 134, 1);
+const greenColor = Color.fromRGBO(47, 103, 23, 1);
+const pinkColor = Color.fromRGBO(220, 168, 170, 1);
+const lightGreenColor = Color.fromRGBO(220, 240, 226, 1);
+const yellorColor = Color.fromRGBO(241, 198, 155, 1);
+const whiteColor = Colors.white;
+const blackColor = Colors.black;
+const gardientColor = LinearGradient(colors: [
+  Color.fromRGBO(184, 144, 78, 1),
+  Color.fromRGBO(244, 213, 163, 1)
+]);
 
 const defaultPadding = 16.0;
