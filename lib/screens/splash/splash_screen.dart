b55871f8ff1cc5b6aa8e01a0311a9f6c554596b4
Thinh.Nguyen <thinh.nguyen@niteco.se>
2022-08-26 10:19:31 +0700

    auth anonymous + splash config

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -6,22 +6,24 @@ import 'package:mybeauty/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
   static String routeName = "/splash";
-
   const SplashScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // You have to call it on your starting screen
     SizeConfig().init(context);
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushNamed(context, BookingScreen.routeName);
+    });
+
     return Scaffold(
-        body: GestureDetector(
-      onTap: () => {Navigator.pushNamed(context, BookingScreen.routeName)},
-      child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: pinkColor,
-        child: Center(
-          child: SvgPicture.asset('assets/images/full-logo.svg'),
-        ),
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: pinkColor,
+      child: Center(
+        child: SvgPicture.asset('assets/images/full-logo.svg'),
       ),
     ));
   }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,4 +4,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
   final user = FirebaseAuth.instance.currentUser;
+
+  Future<void> anonymousLogin() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } on FirebaseAuthException catch (e) {}
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 }
