3712d7bb90f6718c5a0bfa5f4f3c992030bc1164
Thinh.Nguyen <thinh.nguyen@niteco.se>
2022-11-08 17:38:11 +0700

    booking screen

--- lib/screens/bookings/booking_screen.dart
+++ lib/screens/bookings/booking_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:mybeauty/constants.dart';
 import 'package:mybeauty/services/booking_service.dart';
 import 'package:mybeauty/services/auth.dart';
+import 'package:mybeauty/widgets/index.dart';
 import '../../services/models.dart';
+import '../../utils/datetime_utils.dart';
 
 class BookingScreen extends StatefulWidget {
   static String routeName = "/booking";
@@ -39,6 +41,7 @@ class _BookingScreenState extends State<BookingScreen> {
         appBar: AppBar(
           title: const Text(
             'My Bookings',
+            style: TextStyle(color: blackColor, fontWeight: FontWeight.w700),
           ),
           bottom: PreferredSize(
             preferredSize: const Size.square(0),
@@ -49,12 +52,68 @@ class _BookingScreenState extends State<BookingScreen> {
           ),
         ),
         body: Container(
-          margin: const EdgeInsets.only(left: 38, right: 38),
+          margin: const EdgeInsets.only(left: 20, right: 20),
           child: Column(
               children: _bookings.map((AddBookingModel item) {
             return Container(
-                margin: const EdgeInsets.only(bottom: 16.0),
-                child: Text(item.service.name));
+                decoration: const BoxDecoration(
+                    border: Border(
+                        top: BorderSide(color: pinkColor),
+                        bottom: BorderSide(color: pinkColor),
+                        left: BorderSide(color: pinkColor),
+                        right: BorderSide(color: pinkColor))),
+                margin: const EdgeInsets.only(top: 16.0),
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              item.service.name,
+                              style: const TextStyle(
+                                  color: pinkColor,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            const HSpacer(10),
+                            Text(
+                              '/ ${item.service.price}\$',
+                              style: const TextStyle(
+                                  color: blackColor,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          '${DateTimeUtils.getDayOfWeek(item.bookingDate)}, ${DateTimeUtils.getTime(item.bookingTime)}',
+                          style: const TextStyle(color: blackColor),
+                        ),
+                      ],
+                    ),
+                    const VSpacer(10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.person_outline),
+                            const HSpacer(10),
+                            Text(
+                              item.staff.name,
+                              style: const TextStyle(color: blackColor),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          DateTimeUtils.getFullDate(item.bookingDate),
+                          style: const TextStyle(color: blackColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ));
           }).toList()),
         ));
   }
